c1bb1e584d59f6f9325793aa6327c72b3959a70e
kodermax <kodermax@gmail.com>
2016-10-03 23:49:12 +0300

    bottom

---
+++ js/components/bottom/index.js
@@ -0,0 +1,71 @@
+
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Footer, FooterTab, View, Icon} from 'native-base';
+
+import {openDrawer, closeDrawer} from '../../actions/drawer';
+import {popRoute, replaceRoute, replaceOrPushRoute} from '../../actions/route';
+
+class Bottom extends Component {
+
+  static propTypes = {
+    closeDrawer: React.PropTypes.func,
+    replaceRoute: React.PropTypes.func,
+    replaceOrPushRoute: React.PropTypes.func,
+    popRoute: React.PropTypes.func,
+    openDrawer: React.PropTypes.func,
+    page: React.PropTypes.string
+  }
+
+  popRoute() {
+    this.props.popRoute();
+  }
+  navigateTo(route) {
+    this.props.closeDrawer();
+    this.props.replaceOrPushRoute(route);
+  }
+  render() {
+    const {props: {page}} = this;
+
+    return (
+        <Footer>
+          <FooterTab>
+            <View active={page === 'home'} onPress={() => this.navigateTo('home')} >
+              <Icon name='ios-home' />
+            </View>
+            <View active={page === 'works'} onPress={() => this.navigateTo('works')} >
+              <Icon name='logo-instagram' />
+            </View>
+            <View active={page === 'services'} onPress={() => this.navigateTo('services')} >
+              <Icon name='ios-keypad' />
+            </View>
+            <View active={page === 'calc'}>
+              <Icon name='ios-calculator' />
+            </View>
+            <View active={page === 'contacts'} onPress={() => this.navigateTo('contacts')} >
+              <Icon name='ios-call' />
+            </View>
+          </FooterTab>
+        </Footer>
+    );
+  }
+}
+
+function bindAction(dispatch) {
+  return {
+    openDrawer: () => dispatch(openDrawer()),
+    popRoute: () => dispatch(popRoute()),
+    closeDrawer: () => dispatch(closeDrawer()),
+    replaceRoute: route => dispatch(replaceRoute(route)),
+    replaceOrPushRoute: route => dispatch(replaceOrPushRoute(route))
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    name: state.user.name,
+    list: state.list.list
+  };
+}
+
+export default connect(mapStateToProps, bindAction)(Bottom);
---
+++ js/components/services/index.js
@@ -0,0 +1,88 @@
+
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Container, Footer, FooterTab, View, Header, Title, Content, Text, Button, Icon} from 'native-base';
+
+import {openDrawer, closeDrawer} from '../../actions/drawer';
+import {popRoute, replaceRoute, replaceOrPushRoute} from '../../actions/route';
+import myTheme from '../../themes/base-theme';
+
+class Services extends Component {
+
+  static propTypes = {
+    closeDrawer: React.PropTypes.func,
+    replaceRoute: React.PropTypes.func,
+    replaceOrPushRoute: React.PropTypes.func,
+    setIndex: React.PropTypes.func,
+    popRoute: React.PropTypes.func,
+    openDrawer: React.PropTypes.func,
+    name: React.PropTypes.string,
+    index: React.PropTypes.number,
+    list: React.PropTypes.arrayOf(React.PropTypes.string)
+  }
+
+  popRoute() {
+    this.props.popRoute();
+  }
+  navigateTo(route) {
+    this.props.closeDrawer();
+    this.props.replaceOrPushRoute(route);
+  }
+  render() {
+    const {props: {index, list}} = this;
+
+    return (
+      <Container theme={myTheme}>
+        <Header>
+        <Button transparent onPress={this.props.openDrawer}>
+          <Icon name='ios-menu' style={{color: myTheme.toolbarIconColor}} />
+        </Button>
+          <Title>Услуги</Title>
+        </Header>
+        <Content padder>
+          <Text>
+            Услуги
+          </Text>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <View onPress={() => this.navigateTo('home')} >
+              <Icon name='ios-home' />
+            </View>
+            <View onPress={() => this.navigateTo('works')}>
+              <Icon name='logo-instagram' />
+            </View>
+            <View active>
+              <Icon name='ios-keypad' />
+            </View>
+            <View>
+              <Icon name='ios-calculator' />
+            </View>
+            <View active>
+              <Icon name='ios-call' />
+            </View>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
+function bindAction(dispatch) {
+  return {
+    openDrawer: () => dispatch(openDrawer()),
+    popRoute: () => dispatch(popRoute()),
+    closeDrawer: () => dispatch(closeDrawer()),
+    replaceRoute: route => dispatch(replaceRoute(route)),
+    replaceOrPushRoute: route => dispatch(replaceOrPushRoute(route))
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    name: state.user.name,
+    list: state.list.list
+  };
+}
+
+export default connect(mapStateToProps, bindAction)(Services);
